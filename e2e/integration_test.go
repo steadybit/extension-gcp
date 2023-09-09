55e00c36e2a7570205ef37fc6a5d4483d60bc563
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-09-09 13:39:56 +0200

    chore: update dependencies

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -4,48 +4,47 @@
 package e2e
 
 import (
-  "context"
-  "github.com/rs/zerolog/log"
-  "github.com/steadybit/action-kit/go/action_kit_test/e2e"
-  "github.com/steadybit/discovery-kit/go/discovery_kit_api"
-  "github.com/steadybit/extension-gcp/extvm"
-  "github.com/stretchr/testify/require"
-  "testing"
-  "time"
+	"context"
+	"github.com/rs/zerolog/log"
+	"github.com/steadybit/action-kit/go/action_kit_test/e2e"
+	"github.com/steadybit/discovery-kit/go/discovery_kit_api"
+	"github.com/steadybit/extension-gcp/extvm"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
 )
 
 func TestWithMinikube(t *testing.T) {
-  extFactory := e2e.HelmExtensionFactory{
-    Name: "extension-gcp",
-    Port: 8093,
-    ExtraArgs: func(m *e2e.Minikube) []string {
-      return []string{
-        "--set", "logging.level=debug",
-      }
-    },
-  }
+	extFactory := e2e.HelmExtensionFactory{
+		Name: "extension-gcp",
+		Port: 8093,
+		ExtraArgs: func(m *e2e.Minikube) []string {
+			return []string{
+				"--set", "logging.level=debug",
+			}
+		},
+	}
 
-  mOpts := e2e.DefaultMiniKubeOpts
-  mOpts.Runtimes = []e2e.Runtime{e2e.RuntimeDocker}
+	mOpts := e2e.DefaultMiniKubeOpts
+	mOpts.Runtimes = []e2e.Runtime{e2e.RuntimeDocker}
 
-  e2e.WithMinikube(t, mOpts, &extFactory, []e2e.WithMinikubeTestCase{
-    {
-      Name: "discovery",
-      Test: testDiscovery,
-    },
-  })
+	e2e.WithMinikube(t, mOpts, &extFactory, []e2e.WithMinikubeTestCase{
+		{
+			Name: "discovery",
+			Test: testDiscovery,
+		},
+	})
 }
 
 // test the installation of the extension in minikube
 func testDiscovery(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
-  log.Info().Msg("Starting testDiscovery")
-  ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-  defer cancel()
+	log.Info().Msg("Starting testDiscovery")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-  _, err := e2e.PollForTarget(ctx, e, extvm.TargetIDVM, func(target discovery_kit_api.Target) bool {
-    return e2e.HasAttribute(target, "gcp-vm.name", "test")
-  })
-  // we do not have a real gcp vm running, so we expect an error
-  require.Error(t, err)
+	_, err := e2e.PollForTarget(ctx, e, extvm.TargetIDVM, func(target discovery_kit_api.Target) bool {
+		return e2e.HasAttribute(target, "gcp-vm.name", "test")
+	})
+	// we do not have a real gcp vm running, so we expect an error
+	require.Error(t, err)
 }
-
